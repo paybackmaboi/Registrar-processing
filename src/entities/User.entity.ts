07f55bf77c88b14c50e563b8704b19f1f3fe6c92
paybackmaboi <roy.estorco123@gmail.com>
2025-03-05 21:08:32 -0500

    initial commit

--- src/entities/User.entity.ts
+++ src/entities/User.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
 
 @Entity()
-export class User {
+export class Student {
     @PrimaryGeneratedColumn()
     id!: number;
 
